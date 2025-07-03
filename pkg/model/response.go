ad4b5683abcf6c8977b214e298569dd72ad28564
Boris <glazunov2142@icloud.com>
2025-07-03 11:32:45 +0300

    Update response.go

--- pkg/model/response.go
+++ pkg/model/response.go
@@ -4,18 +4,20 @@ import (
 	"bytes"
 	"compress/gzip"
 	"context"
+	"fmt"
 	"github.com/caddyserver/caddy/v2/pkg/config"
 	"github.com/caddyserver/caddy/v2/pkg/list"
 	"math"
 	"math/rand/v2"
 	"net/http"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
 	"unsafe"
 )
 
-const gzipThreshold = 1024 // Minimum body size to apply gzip compression
+const gzipThreshold = 1024 // Minimum body size to apply compress compression
 
 // -- Internal pools for efficient memory management --
 
@@ -24,7 +26,7 @@ var (
 	GzipWriterPool = &sync.Pool{New: func() any {
 		w, err := gzip.NewWriterLevel(nil, gzip.BestSpeed)
 		if err != nil {
-			panic("failed to Init. gzip writer: " + err.Error())
+			panic("failed to Init. compress writer: " + err.Error())
 		}
 		return w
 	}}
@@ -216,4 +218,44 @@ func (r *Response) setUpWeight() int64 {
 // Weight estimates the in-memory size of this response (including dynamic fields).
 func (r *Response) Weight() int64 {
 	return r.weight
+}
+
+func (r *Response) PrintDump() {
+	req := r.Request()
+	data := r.Data()
+
+	// Format request headers
+	reqHeaders := make([]string, 0, len(req.Headers()))
+	for _, header := range req.Headers() {
+		reqHeaders = append(reqHeaders, fmt.Sprintf("%s: %s", header[0], header[1]))
+	}
+
+	// Format data headers
+	dataHeaders := make([]string, 0, len(data.Headers()))
+	for key, values := range data.Headers() {
+		for _, val := range values {
+			dataHeaders = append(dataHeaders, fmt.Sprintf("%s: %s", key, val))
+		}
+	}
+
+	fmt.Printf(
+		"Response Dump {\n"+
+			"  Request:\n"+
+			"    MapKey:   %d\n"+
+			"    ShardKey: %d\n"+
+			"    Query:    %s\n"+
+			"    Headers:\n      %s\n"+
+			"  Data:\n"+
+			"    StatusCode: %d\n"+
+			"    Headers:\n      %s\n"+
+			"    Body: |-\n      %s\n"+
+			"}\n",
+		req.MapKey(),
+		req.ShardKey(),
+		string(req.ToQuery()),
+		strings.Join(reqHeaders, "\n      "),
+		data.StatusCode(),
+		strings.Join(dataHeaders, "\n      "),
+		string(data.Body()),
+	)
 }
